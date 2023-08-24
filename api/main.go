5f59e0a648a1eced5556eecc238f14ca8efca318
VanessaM557 <122654057+VanessaM557@users.noreply.github.com>
2023-08-23 22:29:07 -0400

    Update and rename main.go to api/main.go

--- api/main.go
+++ api/main.go
@@ -1,42 +1,70 @@
 package main
+
 import (
 	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
 	"strings"
-	"github.com/go-redis/redis/v8" 
+	"time"
+	"github.com/go-redis/redis/v8"
 )
 
-var context = context.Background()
-var rdb *redis.Client
+// structs
 
-//Vote struct 
+type AppContext struct {
+	Ctx context.Context
+	Rdb *redis.Client
+}
 
 type Voter struct {
-	ID   string
-	Name string
+	ID        string
+	FirstName string
+	LastName  string
+}
+
+type HealthData struct {
+	TotalAPICalls           int
+	TotalAPICallsWithError  int
+	BootTime                time.Time
+	Uptime                  string
+}
+
+type VoterList struct {
+	Voters     []Voter
+	HealthData HealthData
 }
 
 func main() {
-	rdb = redis.NewClient(&redis.Options{
+	ctx := context.Background()
+	rdb := redis.NewClient(&redis.Options{
 		Addr: "localhost:6379",
-		DB:   0,
 	})
+
+	appContext := &AppContext{
+		Ctx: ctx,
+		Rdb: rdb,
+	}
 
 	http.HandleFunc("/voters", func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
 		case http.MethodGet:
-			handleGetAllVoters(w, r)
+			handleGetAllVoters(appContext, w, r)
 		case http.MethodPost:
-			handleCreateNewVoter(w, r)
-		default:
-			http.Error(w, "Method is not permmitted", http.StatusMethodNotAllowed)
+			handleCreateNewVoter(appContext, w, r)
 		}
 	})
 
 	http.HandleFunc("/voters/", func(w http.ResponseWriter, r *http.Request) {
-		handleGetVoterByID(w, r)
+		id := strings.TrimPrefix(r.URL.Path, "/voters/")
+		switch r.Method {
+		case http.MethodGet:
+			handleGetVoterByID(appContext, w, r, id)
+		case http.MethodPut:
+			handleUpdateVoterByID(appContext, w, r, id)
+		case http.MethodDelete:
+			handleDeleteVoterByID(appContext, w, r, id)
+		}
 	})
 
 	port := 8080
@@ -44,16 +72,15 @@ func main() {
 	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 }
 
-func handleGetAllVoters(w http.ResponseWriter, r *http.Request) {
-	voterKeys, err := rdb.Keys(context, "voter:*").Result()
+func handleGetAllVoters(appContext *AppContext, w http.ResponseWriter, r *http.Request) {
+	voterKeys, err := appContext.Rdb.Keys(appContext.Ctx, "voter:*").Result() // Fix here
 	if err != nil {
 		http.Error(w, "Error retrieving voters", http.StatusInternalServerError)
 		return
 	}
-
 	voters := []Voter{}
 	for _, key := range voterKeys {
-		voterJSON, err := rdb.Get(context, key).Result()
+		voterJSON, err := appContext.Rdb.Get(appContext.Ctx, key).Result() // Fix here
 		if err != nil {
 			http.Error(w, "Error retrieving voter", http.StatusInternalServerError)
 			return
@@ -66,18 +93,16 @@ func handleGetAllVoters(w http.ResponseWriter, r *http.Request) {
 		}
 		voters = append(voters, voter)
 	}
-
 	responseJSON, err := json.Marshal(voters)
 	if err != nil {
 		http.Error(w, "Error encoding response", http.StatusInternalServerError)
 		return
 	}
-
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(responseJSON)
 }
 
-func handleCreateNewVoter(w http.ResponseWriter, r *http.Request) {
+func handleCreateNewVoter(appContext *AppContext, w http.ResponseWriter, r *http.Request) {
 	var voter Voter
 	decoder := json.NewDecoder(r.Body)
 	err := decoder.Decode(&voter)
@@ -85,35 +110,18 @@ func handleCreateNewVoter(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Invalid request body", http.StatusBadRequest)
 		return
 	}
-
 	voterJSON, err := json.Marshal(voter)
 	if err != nil {
 		http.Error(w, "Error encoding voter", http.StatusInternalServerError)
 		return
 	}
-
-	err = rdb.Set(context, "voter:"+voter.ID, voterJSON, 0).Err()
+	err = appContext.Rdb.Set(appContext.Ctx, "voter:"+voter.ID, voterJSON, 0).Err() // Fix here
 	if err != nil {
 		http.Error(w, "Error saving voter", http.StatusInternalServerError)
 		return
 	}
-
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(voterJSON)
 }
 
-func handleGetVoterByID(w http.ResponseWriter, r *http.Request) {
-	id := strings.TrimPrefix(r.URL.Path, "/voters/")
 
-	voterJSON, err := rdb.Get(context, "voter:"+id).Result()
-	if err == redis.Nil {
-		http.Error(w, "Voter cannot be found", http.StatusNotFound)
-		return
-	} else if err != nil {
-		http.Error(w, "Error retrieving voter", http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(voterJSON))
-}
