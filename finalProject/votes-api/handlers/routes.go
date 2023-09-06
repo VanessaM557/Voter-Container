080a3f1845baba7803dae5fdf8feeb79aa011f2b
VanessaM557 <122654057+VanessaM557@users.noreply.github.com>
2023-09-06 19:13:30 -0400

    Create routes.go

---
+++ finalProject/votes-api/handlers/routes.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "github.com/gin-gonic/gin"
+    "github.com/yourusername/votes-api/handlers"
+)
+
+func registerRoutes() *gin.Engine {
+    r := gin.Default()
+
+    // Votes
+    r.POST("/votes", handlers.CreateVote)
+    r.GET("/votes", handlers.GetVotes)
+    r.GET("/votes/:id", handlers.GetVoteByID)
+    r.DELETE("/votes/:id", handlers.DeleteVoteByID)
+
+    return r
+}
