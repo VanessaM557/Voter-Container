4c2a024fb0923e3115d5f50af30dfffd4e76c265
VanessaM557 <122654057+VanessaM557@users.noreply.github.com>
2023-09-06 18:53:03 -0400

    Update and rename finalProject/voter-api/data/models.go to finalProject/voter-api/data/models/voter.go

--- finalProject/voter-api/data/models/voter.go
+++ finalProject/voter-api/data/models/voter.go
@@ -18,17 +18,14 @@ type Voter struct {
 	VoteHistory []VoterPoll
 }
 
-// Returns the full name of the voter.
 func (v *Voter) FullName() string {
 	return fmt.Sprintf("%s %s", v.FirstName, v.LastName)
 }
 
-// Returns the number of polls the voter has participated in.
 func (v *Voter) TotalVotes() int {
 	return len(v.VoteHistory)
 }
 
-// Check if the voter has voted in a specific poll.
 func (v *Voter) HasVotedIn(pollID uint) bool {
 	for _, vp := range v.VoteHistory {
 		if vp.PollID == pollID {
@@ -38,12 +35,10 @@ func (v *Voter) HasVotedIn(pollID uint) bool {
 	return false
 }
 
-// Get the last poll the voter has voted in, if available.
 func (v *Voter) LastVotedPoll() (*VoterPoll, error) {
 	if len(v.VoteHistory) == 0 {
 		return nil, errors.New("voter has no voting history")
 	}
-	// Assuming the VoteHistory is not necessarily sorted, we find the most recent vote date.
 	latestVote := v.VoteHistory[0]
 	for _, vp := range v.VoteHistory {
 		if vp.VoteDate.After(latestVote.VoteDate) {
@@ -53,22 +48,18 @@ func (v *Voter) LastVotedPoll() (*VoterPoll, error) {
 	return &latestVote, nil
 }
 
-// Add a new poll to the voter's voting history.
 func (v *Voter) AddVoterPoll(vp VoterPoll) {
 	v.VoteHistory = append(v.VoteHistory, vp)
 }
 
-// Add multiple polls at once to the voter's voting history.
 func (v *Voter) AddMultipleVoterPolls(vps []VoterPoll) {
 	v.VoteHistory = append(v.VoteHistory, vps...)
 }
 
-// Clear the voting history of a voter.
 func (v *Voter) ClearVoteHistory() {
 	v.VoteHistory = []VoterPoll{}
 }
 
-// Reset the voter's ID.
 func (v *Voter) ResetVoterID(newID uint) {
 	v.VoterID = newID
 }
