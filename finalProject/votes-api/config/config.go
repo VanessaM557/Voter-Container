dcae3b57ecf78e16fec734c1d45ba02fab56543e
VanessaM557 <122654057+VanessaM557@users.noreply.github.com>
2023-09-06 21:16:55 -0400

    Create config.go

---
+++ finalProject/votes-api/config/config.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"log"
+	"os"
+)
+
+type Config struct {
+	RedisHost     string
+	RedisPort     string
+	RedisPassword string
+	APIPort       string
+}
+
+// LoadConfig loads configuration from environment variables, or sets defaults.
+func LoadConfig() *Config {
+	config := &Config{
+		RedisHost:     getEnv("REDIS_HOST", "localhost"),
+		RedisPort:     getEnv("REDIS_PORT", "6379"),
+		RedisPassword: getEnv("REDIS_PASSWORD", ""),
+		APIPort:       getEnv("API_PORT", ":8080"),
+	}
+
+	return config
+}
+func getEnv(key string, defaultValue string) string {
+	value, exists := os.LookupEnv(key)
+	if !exists {
+		log.Printf("Environment variable %s not set, using default: %s", key, defaultValue)
+		return defaultValue
+	}
+	return value
+}
